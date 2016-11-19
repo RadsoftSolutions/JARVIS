85500b146db5185882ce21853a233ec255949673
Arpit Maheshwari <arp19690@gmail.com>
2016-11-19 16:52:26 +0530

    stopping it for 100% charge

--- jarvis/cron.py
+++ jarvis/cron.py
@@ -10,8 +10,8 @@ def check_battery_percent():
     if battery_percent <= 15 and battery_status == 'discharging':
         os.system("say sir, I am at " + str(battery_percent) + " percent charge")
         SystemCommands.message_list_to_speech(message_list)
-    elif battery_percent == 100 and battery_status == 'charged':
-        os.system("say Sir, I am at " + str(battery_percent) + " percent charge. You can remove my power source.")
+    # elif battery_percent == 100 and battery_status == 'charged':
+    #     os.system("say Sir, I am at " + str(battery_percent) + " percent charge. You can remove my power source.")
 
 
 script, keyword = argv
