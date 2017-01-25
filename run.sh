df703961b0ed26cc27d90fefd475ed3866ce015e
Arpit Maheshwari <arp19690@gmail.com>
2017-01-25 11:18:05 +0530

    creating shell script

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "Activating Virtual Environment"
+. /Applications/XAMPP/htdocs/work/svn/JARVIS/venv/bin/activate
+
+echo "Initializing JARVIS"
+python /Applications/XAMPP/htdocs/work/svn/JARVIS/listen.py
+
+echo ""
